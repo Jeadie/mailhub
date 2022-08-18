8493e5bf6bd629fe2d8c574f1a85abb86b0227f4
jeadie <jackeadie@duck.com>
2022-08-18 20:21:17 +1000

    add POST json handling

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mailhub
+
+
+### Testing POST
+```shell
+curl --header "Content-Type: application/json" \
+     --request POST \
+      --data '{
+        "Phone": "61412345678",
+        "Content": "Hello World",
+        "Date": "1970-01-01 12:00:01"
+        }' \
+  http://0.0.0.0:8080/sms/phone_number
+```
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"encoding/xml"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
+
+type SmsMessage struct {
+	XMLName  xml.Name `json:"Message,omitempty"`
+	Smstat   uint     `json:"Smstat,omitempty"`
+	Index    uint     `json:"Index,omitempty"`
+	Phone    string   `json:"Phone,omitempty"`
+	Content  string   `json:"Content,omitempty"`
+	Date     string   `json:"Date,omitempty"`
+	Sca      any      `json:"Sca,omitempty"`
+	SaveType uint     `json:"SaveType,omitempty"`
+	Priority uint     `json:"Priority,omitempty,omitempty"`
+	SmsType  uint     `json:"SmsType,omitempty"`
+}
 
 func main() {
 	r := gin.Default()
@@ -15,8 +29,13 @@ func main() {
 	})
 
 	r.POST("/sms/:name", func(c *gin.Context) {
+		var sms SmsMessage
+		if err := c.ShouldBindJSON(&sms); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
+		c.String(http.StatusOK, "Hello %s. You received an SMS from %s, saying %s", name, sms.Phone, sms.Content)
 	})
 
 	r.Run()
