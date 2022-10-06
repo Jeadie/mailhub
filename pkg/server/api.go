548300b78f7fbf0b0d1568a0c61fb05dd3fe619f
jeadie <jackeadie@duck.com>
2022-10-06 14:57:27 +1000

    temp changes from MacOS

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -1,52 +1,52 @@
 package server
 
 import (
-	"github.com/Jeadie/mailhub/pkg/db"
-	"github.com/gin-gonic/gin"
-	"net/http"
+    "github.com/Jeadie/mailhub/pkg/db"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 // ConstructEndpoints on top of a gin Engine and a dao object.
 func ConstructEndpoints(r *gin.Engine, dao db.Dao) {
 
-	r.GET("/mailhub", func(c *gin.Context) {
-		smss, err := dao.GetAllSmss()
-		if err != nil {
-			c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"count": len(smss),
-				"smss":  smss,
-			})
-		}
-	})
+    r.GET("/mailhub", func(c *gin.Context) {
+        smss, err := dao.GetAllSmss()
+        if err != nil {
+            c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
+        } else {
+            c.JSON(http.StatusOK, gin.H{
+                "count": len(smss),
+                "smss":  smss,
+            })
+        }
+    })
 
-	r.GET("/sms/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		smss, err := dao.GetSmssTo(name)
+    r.GET("/sms/:name", func(c *gin.Context) {
+        name := c.Param("name")
+        smss, err := dao.GetSmssTo(name)
 
-		if err != nil {
-			c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"count": len(smss),
-				"smss":  smss,
-			})
-		}
-	})
+        if err != nil {
+            c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
+        } else {
+            c.JSON(http.StatusOK, gin.H{
+                "count": len(smss),
+                "smss":  smss,
+            })
+        }
+    })
 
-	r.POST("/sms/:name", func(c *gin.Context) {
-		var sms db.SmsMessage
-		if err := c.ShouldBindJSON(&sms); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		name := c.Param("name")
-		err := dao.Save(name, sms)
-		if err != nil {
-			c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
-		} else {
-			c.String(http.StatusOK, "Hello %s. You received an SMS from %s, saying %s", name, sms.Phone, sms.Content)
-		}
-	})
+    r.POST("/sms/:name", func(c *gin.Context) {
+        var sms db.SmsMessage
+        if err := c.ShouldBindJSON(&sms); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+        name := c.Param("name")
+        err := dao.Save(name, sms)
+        if err != nil {
+            c.JSON(http.StatusTeapot, gin.H{"error": err.Error()})
+        } else {
+            c.String(http.StatusOK, "Hello %s. You received an SMS from %s, saying %s", name, sms.Phone, sms.Content)
+        }
+    })
 }
